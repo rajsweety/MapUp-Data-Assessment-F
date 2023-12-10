c525633d9793172e3d391bfc05b6f1d48025c592
Sweety <77089312+rajsweety@users.noreply.github.com>
2023-12-10 11:29:19 +0530

    Update python_task_1.py

--- templates/python_task_1.py
+++ templates/python_task_1.py
@@ -1,20 +1,37 @@
 import pandas as pd
 
 
-def generate_car_matrix(df)->pd.DataFrame:
-    """
-    Creates a DataFrame  for id combinations.
+import numpy as np
 
-    Args:
-        df (pandas.DataFrame)
+def generate_car_matrix(df: pd.DataFrame) -> pd.DataFrame:
+   """
+   Creates a DataFrame for id combinations.
 
-    Returns:
-        pandas.DataFrame: Matrix generated with 'car' values, 
-                          where 'id_1' and 'id_2' are used as indices and columns respectively.
-    """
-    # Write your logic here
+   Args:
+       df (pandas.DataFrame)
 
-    return df
+   Returns:
+       pandas.DataFrame: Matrix generated with 'car' values, 
+                        where 'id_1' and 'id_2' are used as indices and columns respectively.
+   """
+   # Set 'id_1' as the index of our DataFrame
+   df = df.set_index('id_1')
+
+   # Pivot our DataFrame to get the desired structure
+   df = df.pivot(columns='id_2', values='car')
+
+   # Set the diagonal values to 0
+   np.fill_diagonal(df.values, 0)
+
+   return df
+
+# Read the CSV file from the URL
+url = 'https://raw.githubusercontent.com/mapup/MapUp-Data-Assessment-F/main/datasets/dataset-1.csv'
+df = pd.read_csv(url)
+
+# Call the function
+car_matrix = generate_car_matrix(df)
+print(car_matrix)
 
 
 def get_type_count(df)->dict:
