8230cc3212fb14a48cdcb41acf2ba788db90ec49
Sweety <77089312+rajsweety@users.noreply.github.com>
2023-12-10 20:58:15 +0530

    Update python_task_2.py

--- templates/python_task_2.py
+++ templates/python_task_2.py
@@ -11,28 +11,7 @@ def calculate_distance_matrix(df)->pd.DataFrame():
     Returns:
         pandas.DataFrame: Distance matrix
     """
-    # Ensure 'id_start', 'id_end', and 'distance' columns are present
-    if 'id_start' not in df.columns or 'id_end' not in df.columns or 'distance' not in df.columns:
-        raise ValueError("Input dataset is missing required columns.")
-
-    # Create a pivot table to get bidirectional distances
-    distance_matrix = df.pivot(index='id_start', columns='id_end', values='distance').fillna(0)
-
-    # Make the matrix symmetric
-    distance_matrix = distance_matrix + distance_matrix.T
-
-    # Set diagonal values to 0
-    distance_matrix.values[[range(len(distance_matrix))]*2] = 0
-
-    return distance_matrix
-
-# Load the dataset from the provided GitHub URL
-url = 'https://raw.githubusercontent.com/mapup/MapUp-Data-Assessment-F/main/datasets/dataset-3.csv'
-dataset = pd.read_csv(url)
-
-# Call the function with the dataset
-result_distance_matrix = calculate_distance_matrix(dataset)
-print(result_distance_matrix)
+    # Write your logic here
 
     return df
 
@@ -47,22 +26,7 @@ def unroll_distance_matrix(df)->pd.DataFrame():
     Returns:
         pandas.DataFrame: Unrolled DataFrame containing columns 'id_start', 'id_end', and 'distance'.
     """
-    # Ensure the input is a valid DataFrame
-    if not isinstance(distance_matrix, pd.DataFrame):
-        raise ValueError("Input must be a pandas DataFrame.")
-
-    # Reshape the distance matrix to long format
-    unrolled_df = distance_matrix.stack().reset_index()
-    unrolled_df.columns = ['id_start', 'id_end', 'distance']
-
-    # Exclude rows where id_start is equal to id_end
-    unrolled_df = unrolled_df[unrolled_df['id_start'] != unrolled_df['id_end']]
-
-    return unrolled_df
-
-# Call the function with the distance matrix from Question 1
-result_unrolled_df = unroll_distance_matrix(result_distance_matrix)
-print(result_unrolled_df)
+    # Write your logic here
 
     return df
 
@@ -79,33 +43,8 @@ def find_ids_within_ten_percentage_threshold(df, reference_id)->pd.DataFrame():
         pandas.DataFrame: DataFrame with IDs whose average distance is within the specified percentage threshold
                           of the reference ID's average distance.
     """
-    # Ensure the input is a valid DataFrame
-    if not isinstance(df, pd.DataFrame):
-        raise ValueError("Input must be a pandas DataFrame.")
+    # Write your logic here
 
-    # Filter DataFrame based on the reference_id
-    reference_data = df[df['id_start'] == reference_id]
-
-    # Calculate the average distance for the reference_id
-    reference_avg_distance = reference_data['distance'].mean()
-
-    # Calculate the percentage threshold
-    threshold = 0.1  # 10%
-
-    # Find IDs within the percentage threshold
-    result_series = df.groupby('id_start')['distance'].mean()
-    result_series = result_series[(result_series >= (1 - threshold) * reference_avg_distance) &
-                                  (result_series <= (1 + threshold) * reference_avg_distance)]
-
-    # Sort the result Series by average distance
-    result_series = result_series.sort_values()
-
-    return result_series
-
-# Call the function with the unrolled DataFrame from Question 2 and a reference ID
-reference_id = 1  # Replace with the desired reference ID
-result_within_threshold = find_ids_within_ten_percentage_threshold(result_unrolled_df, reference_id)
-print(result_within_threshold)
     return df
 
 
@@ -119,22 +58,7 @@ def calculate_toll_rate(df)->pd.DataFrame():
     Returns:
         pandas.DataFrame
     """
-    # Ensure the input is a valid DataFrame
-    if not isinstance(df, pd.DataFrame):
-        raise ValueError("Input must be a pandas DataFrame.")
-
-    # Define rate coefficients for each vehicle type
-    rate_coefficients = {'moto': 0.8, 'car': 1.2, 'rv': 1.5, 'bus': 2.2, 'truck': 3.6}
-
-    # Add columns for toll rates based on vehicle type
-    for vehicle_type, rate_coefficient in rate_coefficients.items():
-        df[vehicle_type] = df['distance'] * rate_coefficient
-
-    return df
-
-# Call the function with the unrolled DataFrame from Question 2
-result_with_toll_rates = calculate_toll_rate(result_unrolled_df)
-print(result_with_toll_rates)
+    # Wrie your logic here
 
     return df
 
